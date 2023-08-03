b95f0cb3ccc2f8a1ade06fff93fc6ecda9c91072
Hrustch <nik.stalker47@gmail.com>
2023-08-03 13:06:56 +0300

    DONE database connection set

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require('./app')
+const mongoose = require('mongoose')
+const DB_HOST = "mongodb+srv://NikDen:213233@cluster0.ea1an8u.mongodb.net/Contacts_reader?retryWrites=true&w=majority"
+mongoose.set('strictQuery', false)
+mongoose.connect(DB_HOST)
+  .then(()=>{
+    console.log("Database connect success!")
+    app.listen(3000)
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
